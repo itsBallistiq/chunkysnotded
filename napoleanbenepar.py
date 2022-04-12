4d18ee6bfe1cae4ed66292d0adfb8cb991eebadc
itsBallistiq <InfinitEmojiz@gmail.com>
2022-04-12 16:22:59 -0700

    Version 0.0.8

--- napoleanbenepar.py
+++ napoleanbenepar.py
@@ -1,23 +1,23 @@
 import benepar, nltk, spacy
 import ssl
 
-
-# Used to download language models
-# try:
-#     _create_unverified_https_context = ssl._create_unverified_context
-# except AttributeError:
-#     pass
-# else:
-#     ssl._create_default_https_context = _create_unverified_https_context
-# benepar.download('benepar_fr2') # French Model
+#Used to download language models
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+benepar.download('benepar_fr2') # French Model
 
 parser = benepar.Parser("benepar_fr2")
-# nlp = spacy.load('fr_core_news_lg')
+#nlp = spacy.load('fr_core_news_lg') <--- did not work on my computer, next line did work with command python3 -m pip install fr_core_news_md
+nlp = spacy.load('fr_core_news_md')
 
-# if spacy.__version__.startswith('2'):
-#     nlp.add_pipe(benepar.BeneparComponent("benepar_fr2"))
-# else:
-#     nlp.add_pipe("benepar", config={"model": "benepar_fr2"})
+if spacy.__version__.startswith('2'):
+    nlp.add_pipe(benepar.BeneparComponent("benepar_fr2"))
+else:
+	nlp.add_pipe("benepar", config={"model": "benepar_fr2"})
 
 sentence = "Eddie adorer manger des pommes et tu aimer manger des pommes"
 sentence_words = sentence.split(" ")
